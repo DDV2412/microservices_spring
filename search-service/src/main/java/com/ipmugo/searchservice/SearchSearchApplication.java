2bc2b38e936e2bba1bb9e1628a4278e7dc4ba16c
DDV2412 <dhyanputra24@gmail.com>
2023-02-05 10:10:04 +0700

    Add Docker Configuration

---
+++ search-service/src/main/java/com/ipmugo/searchservice/SearchSearchApplication.java
@@ -0,0 +1,11 @@
+package com.ipmugo.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchSearchApplication.class, args);
+    }
+}
